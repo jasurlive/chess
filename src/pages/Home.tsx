2c3d3911c2ea66514d3d71aead2619846afc35ac
jasurlive <jasur@graduate.org>
2026-08-29 12:35:26 +0500

    pawn icon added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="home-page">
       <header className="header-home">
-        <Link to="/">uzChess.vercel.app</Link>
+      <Link to="/"><SiChessdotcom />uzChess.vercel.app</Link>
       </header>
       <ul className="links-container-home">
         <li className="links-grid-home">
